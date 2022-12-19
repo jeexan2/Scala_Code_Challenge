c091bd5c199237ef9268c47d734235c5b4403bb8
jeexan2 <jeexan2@gmail.com>
2022-12-19 06:05:27 -0800

    colored blocks stack solution

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,11 @@
+import colored_block.ColoredBlocks.stack
+import doodle.image.syntax.all.ImageOps
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
+
   }
+
+
 }
---
+++ src/test/scala/colored_block/ColoredBlocksSuite.scala
@@ -0,0 +1,21 @@
+package colored_block
+
+import munit._
+import doodle.image.Image
+
+class ColoredBlocksSuite extends FunSuite {
+  test("stack correctly constructs a stack of blocks") {
+    assertEquals(ColoredBlocks.stack(0), Image.empty)
+    assertEquals(ColoredBlocks.stack(1), ColoredBlocks.block.above(Image.empty))
+    assertEquals(
+      ColoredBlocks.stack(2),
+      ColoredBlocks.block.above(ColoredBlocks.block.above(Image.empty))
+    )
+    assertEquals(
+      ColoredBlocks.stack(3),
+      ColoredBlocks.block.above(
+        ColoredBlocks.block.above(ColoredBlocks.block.above(Image.empty))
+      )
+    )
+  }
+}
