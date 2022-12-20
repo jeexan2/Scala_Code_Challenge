658e0c051d462153d28f36031383b50dec5bb57e
jeexan2 <jeexan2@gmail.com>
2022-12-20 23:39:59 +0700

    adding alternate soln for Snake to Camel Case

--- src/main/scala/snake_camel_case/SnakeToCamelCase.scala
+++ src/main/scala/snake_camel_case/SnakeToCamelCase.scala
@@ -6,5 +6,14 @@ object SnakeToCamelCase {
   def snakeToCamelCase(s: String): String = {
     val snakeWordToRegex = "_(.)".r
     snakeWordToRegex.replaceAllIn(s, m => m.group(1).toUpperCase)
+
+  }
+
+  def snakeToCamelCaseAlternateLogic(s:String) =
+    s.split("_").map(_.capitalize).mkString("")
+
+  def main(args: Array[String]): Unit = {
+    println(snakeToCamelCase("snake_case"))
+    println(snakeToCamelCaseAlternateLogic("snake_case"))
   }
 }
