b64194427ecda60c4fa14fe117bc3dc8b453ec64
jeexan2 <jeexan2@gmail.com>
2022-12-23 00:42:04 +0700

    reservoir sampling addition

---
+++ src/test/scala/reservoir_sampling/ReservoirSamplingSuite.scala
@@ -0,0 +1,20 @@
+package reservoir_sampling
+
+import munit.FunSuite
+
+class ReservoirSamplingSuite extends FunSuite{
+
+  test("sampling empty list returns None") {
+    assertEquals(ReservoirSampling.sample(List.empty[Int]), None)
+  }
+
+  test("sampling one element list returns single element") {
+    assertEquals(ReservoirSampling.sample(List(1)), Some(1))
+  }
+
+  test("sampling larger list returns an element within the list") {
+    val Some(result) = ReservoirSampling.sample(List(0, 1, 2, 3, 4))
+    assert(result >= 0 && result <= 4, s"Result is $result")
+  }
+
+}
